a923105a9ae82eb6f35309384dda7e41f136acc4
KAMAL ARAF <14530113@itisrossi.vi.it>
2025-11-29 11:00:20 +0100

    logica fatta con gli array

--- test_base.py
+++ test_base.py
@@ -1,11 +1,8 @@
-pos_x = 0
-pos_y = 0
-print("posizione iniziale: ",pos_x,";",pos_y)
+pos = [0,0]
+nuova_pos = [0,0]
+movimento = [0,0]
 
-nuova_pos_x = 0
-nuova_pos_y = 0
-print("posizione successiva: ",nuova_pos_x,";",nuova_pos_y)
+for i in range(len(movimento)):
+    movimento[i] = nuova_pos[i] - pos[i]
 
-movimento_x = nuova_pos_x - pos_x
-movimento_y = nuova_pos_y - pos_y
-print("spostato di: ",movimento_x,";",movimento_y)
+print(movimento)
