82890ce2ca5f84b0d955509648543e372fb54773
KAMAL ARAF <14530113@itisrossi.vi.it>
2025-11-29 10:38:13 +0100

    base del funzionamento

--- test_base.py
+++ test_base.py
@@ -1,1 +1,11 @@
+pos_x = 0
+pos_y = 0
+print("posizione iniziale: ",pos_x,";",pos_y)
 
+nuova_pos_x = 0
+nuova_pos_y = 0
+print("posizione successiva: ",nuova_pos_x,";",nuova_pos_y)
+
+movimento_x = nuova_pos_x - pos_x
+movimento_y = nuova_pos_y - pos_y
+print("spostato di: ",movimento_x,";",movimento_y)
